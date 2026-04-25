fb99376051d7b222eef213bd69a268a0524c1847
gopaladhikari <gopuadks@gmail.com>
2026-04-25 23:24:48 +0545

    initialized backeend rojject, setup eslint, prettier and typpescript

---
+++ server/eslint.config.ts
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  { ignores: ["node_modules/", "dist/"] },
+
+  // Base JS & TS configs
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "prefer-const": "error",
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
