51c81697b599cfb97df9cf37b3b6b5ab4e6f0d55
gopaladhikari <gopuadks@gmail.com>
2026-05-01 07:21:28 +0545

    cloudinary and multer

---
+++ server/src/middlewares/multer.middleware.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, "./public");
+  },
+  filename: function (_req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });
---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+  secure: true,
+});
+
+export const uploadMedia = async (mediaPath: string) => {
+  try {
+    const result = await cloudinary.uploader.upload(mediaPath, {
+      resource_type: "auto",
+    });
+    console.log(result);
+    return result.public_id;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const deleteVideo = async (mediaId: string) => {
+  try {
+    await cloudinary.uploader.destroy(mediaId, {
+      resource_type: "video",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
