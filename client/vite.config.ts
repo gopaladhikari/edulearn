091ec791cfbcd35255c21f6373811b392f508fae
gopaladhikari <gopuadks@gmail.com>
2026-05-11 11:18:29 +0545

    added netlify redirect

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,38 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "node:fs";
+import path from "node:path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
+  const apiUrl = env.VITE_API_URL || "http://localhost:8080";
+
   return {
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      {
+        name: "generate-redirects",
+        closeBundle() {
+          const outDir = path.resolve(process.cwd(), "build/client");
+          const fileContent = `/api/*  ${apiUrl}/:splat  200!\n`;
+
+          if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
+
+          fs.writeFileSync(path.join(outDir, "_redirects"), fileContent);
+        },
+      },
+    ],
     server: {
       port: 5173,
       open: true,
 
       proxy: {
         "/api": {
-          target: env.VITE_API_URL || "http://localhost:8080",
+          target: apiUrl,
           changeOrigin: true,
         },
       },
