e52acd98b372ddc863670d0ca710a6d3d18c2776
gopaladhikari <gopuadks@gmail.com>
2026-05-14 12:14:30 +0545

    protected user routes

--- client/app/routes/user/layout.tsx
+++ client/app/routes/user/layout.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { Loading } from "~/components/loading";
+import { useUserStore } from "~/store/userStore";
 
 export default function Layout() {
-  return (
-    <main>
-      <Outlet />
-    </main>
-  );
+  const isAuthenticated = useUserStore((state) => state.isAuthenticated);
+
+  const isAuthChecked = useUserStore((state) => state.isAuthChecked);
+
+  if (!isAuthChecked) return <Loading />;
+
+  if (!isAuthenticated) return <Navigate to="/?redirect=logout" />;
+
+  return <Outlet />;
 }
